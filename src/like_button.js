422aba5b9d2703ff8b901dcc333e79a0cac5923a
yexiang <yc2523@cornell.edu>
2019-08-13 15:33:33 -0400

    Reverted

---
+++ src/like_button.js
@@ -0,0 +1,48 @@
+'use strict';
+
+// import logo from './logo.svg';
+// import './App.css';
+
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked this.';
+    }
+
+    return e(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Like'
+    );
+  }
+}
+
+
+// class App extends React.Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <h1 className="App-title">Welcome to React</h1>
+//         </header>
+//         <p className="App-intro">
+//           To get started, edit <code>src/App.js</code> and save to reload.
+//         </p>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
+// export default LikeButton;
+
+const domContainer = document.getElementById("test");
+ReactDOM.render(e(LikeButton), domContainer);
